c78177b6ffd56b8cbe4ecb1f6be2561f65dd4e77
joao-ramos-a22309710 <a22309710@alunos.ulht.pt>
2024-11-09 14:42:34 +0000

    Lab8

---
+++ lab8/index_Ex1_Java.js
@@ -0,0 +1,81 @@
+
+const mudarFrase = document.getElementById('mudar-frase');
+
+mudarFrase.addEventListener('mouseover', () => {
+    mudarFrase.textContent = "Obrigado por passares!";
+  });
+  
+  mudarFrase.addEventListener('mouseout', () => {
+    mudarFrase.textContent = "Passe por aqui!";
+  });
+
+
+  const texto = document.getElementById('texto');
+
+ 
+  document.querySelectorAll('button[data-color]').forEach((button) => {
+      button.addEventListener('click', () => {
+          const cor = button.dataset.color; 
+          texto.style.color = cor; 
+      });
+  });
+  
+    
+
+const inputBox = document.getElementById('box-texto');
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+inputBox.addEventListener('input', function() {
+    if (this.value.length > 0) {
+        this.style.backgroundColor = getRandomColor(); 
+    } else {
+        this.style.backgroundColor = ''; 
+    }
+});
+
+document.getElementById('cor-input').addEventListener('change', function() {
+    const corInput = this.value;
+    if (corInput) {
+        document.body.style.backgroundColor = corInput;
+    }
+})
+
+
+
+let count = 0; 
+const botaoConta = document.querySelector('#conta');
+const elementoContador = document.querySelector('#count');
+botaoConta.addEventListener('click', function() {
+    count++;  
+    elementoContador.textContent = `${count}`;  
+});
+
+
+document.querySelector('form').onsubmit = (e) => {
+    e.preventDefault();
+    
+    const nome = document.querySelector('#nome').value;
+    const idade = document.querySelector('#idade').value;
+    
+    const mensagem = `Olá, o/a ${nome} tem ${idade} anos!`;
+    
+    document.querySelector('#mensagem').textContent = mensagem;
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    let counter = 0; 
+    function count() {
+        counter++; 
+        document.getElementById("contador").textContent = counter; 
+    }
+    setInterval(count, 1000);
+});
